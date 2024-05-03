0b8d4941a3b1f71d6752ea633ad5f5ddc0f9259f
Matheus Pinheiro <141381732+Siateus@users.noreply.github.com>
2024-05-03 16:29:51 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -17,6 +17,6 @@ public class ContaTerminal {
 
         System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "
                 +agencia+", conta "+numeroAgencia+" e seu saldo "+saldo+ "já está disponível para saque.");
-        scanner.close():
+        scanner.close();
     }
 }
